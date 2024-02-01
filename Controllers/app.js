55cd32c5933cc8739ff5cf166b497e32a6daf88c
Dezső Tamás <dezsotamas@turr.hu>
2024-02-01 09:26:32 +0100

    css

--- Controllers/app.js
+++ Controllers/app.js
@@ -1,8 +1,10 @@
 var app = angular.module('MunkaIdoApp' ['ngRoute', 'ngNotify']);
 
 app.run(function($rootScope, $window){
-    $rootScope.appTitle('Munka Idő App');
+    $rootScope.appTitle ='Munka Idő App';
 });
+
+
 
 app.config(function($routeProvider){
 
