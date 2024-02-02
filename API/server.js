d257d5bcb96a7c50dbfa11e2fa9a50e60cca343b
Dezső Tamás <dezsotamas@turr.hu>
2024-02-02 13:18:18 +0100

    2. óra vége

--- API/server.js
+++ API/server.js
@@ -6,6 +6,8 @@ const app = express();
 const post = porcess.env.PORT;
 
 var db_modul = require('./module/database');
+const { Router } = require('express');
+const { createPool } = require('mysql');
 
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
@@ -16,3 +18,62 @@ app.use('/db', db_module);
 app.listen(port, () =>{
     console.log(`Server listening on port ${port}...`);
 });
+
+
+////      ////
+//   CRUD   //
+////      ////
+
+
+
+// CREATE
+    // add user
+router.post(':/employees', (req, res)=>{
+    pool.query(`INSERT INTO employees ID, name, address, position, pricePerHour VALUES (colum)`,(err, results)=>{
+        sendResults(table, err, results, req, res);
+    });
+
+})
+
+
+
+// add WORKTIME
+router.post(':/worktimes', (req, res)=>{
+    pool.query(`INSERT INTO worktimes ID, empID, date, position, start, end   VALUES (colum)`,(err, results)=>{
+        sendResults(table, err, results, req, res);
+    });
+
+})
+
+// READ
+
+    // GET all employees
+router.get(':employees',(req, res) => {
+    let table = req.params.table;
+    createPool.query(`SELECT * FROM employees`, err, results=>{
+        sendResults(table, err, results, req, res);
+    });
+});
+
+// GET all worktimes
+router.get(':worktimes',(req, res) => {
+    let table = req.params.table;
+    createPool.query(`SELECT * FROM worktimes`, err, results=>{
+        sendResults(table, err, results, req, res);
+    });
+});
+
+
+
+
+
+
+// UPDATE
+// update employees
+router.patch(`UPDATE employees set`,(req, res)=>{
+
+});
+
+// DELETE
+
+
