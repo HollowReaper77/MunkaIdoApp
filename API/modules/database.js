339ffc740fa1b0f4e42b4b81969a3e70c63787b3
Dezső Tamás <dezsotamas@turr.hu>
2024-02-02 13:19:19 +0100

    2. óra vége tényleg

--- API/modules/database.js
+++ API/modules/database.js
@@ -11,8 +11,62 @@ var pool = mysql.createPool({
     timezone: 'UTC'
 })
 
-// ENDPOINTS
 
-// get all records
+////      ////
+//   CRUD   //
+////      ////
 
-router.get()
+
+
+// CREATE
+    // add user
+    router.post(':/employees', (req, res)=>{
+        pool.query(`INSERT INTO employees ID, name, address, position, pricePerHour VALUES (colum)`,(err, results)=>{
+            sendResults(table, err, results, req, res);
+        });
+    
+    })
+    
+    
+    
+    // add WORKTIME
+    router.post(':/worktimes', (req, res)=>{
+        pool.query(`INSERT INTO worktimes ID, empID, date, position, start, end   VALUES (colum)`,(err, results)=>{
+            sendResults(table, err, results, req, res);
+        });
+    
+    })
+    
+    // READ
+    
+        // GET all employees
+    router.get(':employees',(req, res) => {
+        let table = req.params.table;
+        createPool.query(`SELECT * FROM employees`, err, results=>{
+            sendResults(table, err, results, req, res);
+        });
+    });
+    
+    // GET all worktimes
+    router.get(':worktimes',(req, res) => {
+        let table = req.params.table;
+        createPool.query(`SELECT * FROM worktimes`, err, results=>{
+            sendResults(table, err, results, req, res);
+        });
+    });
+    
+    
+    
+    
+    
+    
+    // UPDATE
+    // update employees
+    router.patch(`UPDATE employees set`,(req, res)=>{
+    
+    });
+    
+    // DELETE
+    
+    
+    
